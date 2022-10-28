403872791856ded03b9c5dbf3ab968c9683aeb9f
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-10-28 20:51:41 +0900

    대소문자 변환

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public int solution(String str, char t) {
-        int answer =0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t);
-
-        for (char a : str.toCharArray()) {
-            if (a == t) {
-                answer++;
-            }
-        }
+    public String solution(String str) {
 
         return answer;
     }
@@ -20,9 +11,8 @@ public class Main {
 
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
-        char t = kb.next().charAt(0);
 
-        System.out.println(T.solution(str, t));
+        System.out.println(T.solution(str));
 
     }
 }
