e7d7156e6c54996180b6f7f5fe307732a6240daf
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-11-02 21:36:16 +0900

    암호

--- src/Password.java
+++ src/Password.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Password {
     public String solution(int n, String s) {
         String answer = "";
         for (int i = 0; i < n; i++) {
@@ -15,7 +15,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
+        Password T = new Password();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         String str = kb.next();
