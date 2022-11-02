7abbd6907d0d75c0e36641be6ef35cb8ab3c4297
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-11-02 21:06:28 +0900

    문자열 압축

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class StringCompression {
     public String solution(String s) {
         String answer = "";
         s = s + " ";
@@ -21,7 +21,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
+        StringCompression T = new StringCompression();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
         System.out.println(T.solution(str));
