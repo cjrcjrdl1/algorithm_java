18cd9e309844769c9e017febd3e1f3923765527f
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-11-04 14:08:35 +0900

    문제1번

--- src/study_string/exam1.java
+++ src/study_string/exam1.java
@@ -1,6 +1,8 @@
+package study_string;
+
 import java.util.Scanner;
 
-public class Main {
+public class exam1 {
     public int solution(String str, char c) {
         int answer = 0;
         str = str.toUpperCase();
@@ -15,7 +17,7 @@ public class Main {
         return answer;
     }
     public static void main(String[] args) {
-        Main T = new Main();
+        exam1 T = new exam1();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
         char c = scanner.next().charAt(0);
