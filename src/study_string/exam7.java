eeb9bec51d0797e456d25c9742730ee0990ea2aa
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-11-11 10:11:55 +0900

    문제7번

--- src/study_string/exam7.java
+++ src/study_string/exam7.java
@@ -1,25 +1,27 @@
-import java.util.ArrayList;
+package study_string;
+
 import java.util.Scanner;
 
-public class Main {
+public class exam7 {
     public String solution(String str) {
-//        String answer = "YES";
-//        str = str.toUpperCase();
+        String answer = "NO";
+        str = str.toUpperCase();
 //        int len = str.length();
 //        for (int i = 0; i < len / 2; i++) {
 //            if (str.charAt(i) != str.charAt(len - 1 - i)) {
-//                return "NO";
+//                answer = "NO";
 //            }
 //        }
-        String answer = "NO";
+
         String tmp = new StringBuilder(str).reverse().toString();
-        if (str.equalsIgnoreCase(tmp)) {
+        if (str.equalsIgnoreCase(tmp)) {  //equalsIgnoreCase 이것은 대소문자 구분 무시
             answer = "YES";
         }
+
         return answer;
     }
     public static void main(String[] args) {
-        Main T = new Main();
+        exam7 T = new exam7();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
         System.out.println(T.solution(str));
