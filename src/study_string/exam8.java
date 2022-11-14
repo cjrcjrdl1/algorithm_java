7b2cd45a09fb80298ad7468b450b50417b5358cf
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-11-14 21:37:11 +0900

    문제8번

--- src/study_string/exam8.java
+++ src/study_string/exam8.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
+package study_string;
+
 import java.util.Scanner;
 
-public class Main {
+public class exam8 {
     public String solution(String s) {
         String answer = "NO";
         s = s.toUpperCase().replaceAll("[^A-Z]", "");
@@ -11,8 +12,9 @@ public class Main {
         }
         return answer;
     }
+
     public static void main(String[] args) {
-        Main T = new Main();
+        exam8 T = new exam8();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         System.out.println(T.solution(str));
