9363495cb645d72a154d11758a3b72a31b3bbfdb
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-11 22:50:54 +0900

    백준 25083번

---
+++ src/baekjoon/n_25083.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+public class n_25083 {
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
+
+
