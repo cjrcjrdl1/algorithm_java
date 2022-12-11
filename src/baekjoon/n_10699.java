02fc8cede027452e96065a8affcde2dcef45565b
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-11 22:45:40 +0900

    백준 10699번

---
+++ src/baekjoon/n_10699.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Scanner;
+
+public class n_10699 {
+//    public String solution() {
+//
+//    }
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now().getYear() + "-" + LocalDateTime.now().getMonthValue() + "-" + LocalDateTime.now().getDayOfMonth());
+    }
+}
+
+
