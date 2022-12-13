1e0bb97e35db12c5095361d8ad997d11bb0bead2
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-13 13:41:28 +0900

    백준 2420번

---
+++ src/baekjoon/n_2420.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class n_2420 {
+
+
+    public static void main(String[] args) {
+//        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+
+        System.out.println(Math.abs(a-b));
+    }
+}
+
+
