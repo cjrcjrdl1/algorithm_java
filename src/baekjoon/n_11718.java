1204ff019d10fd4d8bc57221963878a309500b98
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-16 13:35:28 +0900

    백준 11718번

---
+++ src/baekjoon/n_11718.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class n_11718 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+
+        }
+    }
+}
+
+
