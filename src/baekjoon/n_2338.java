b3dddbee1b24578b51e8bf7359c66ef43ec14298
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-19 22:17:18 +0900

    백준 2338번

---
+++ src/baekjoon/n_2338.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class n_2338 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
+
+
