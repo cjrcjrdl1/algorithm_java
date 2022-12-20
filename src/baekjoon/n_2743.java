e7d1169427b73f6049a7a05b110ece0788db016f
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-21 00:01:52 +0900

    백준 2743번

---
+++ src/baekjoon/n_2743.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class n_2743 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        System.out.println(s.length());
+    }
+}
+
+
