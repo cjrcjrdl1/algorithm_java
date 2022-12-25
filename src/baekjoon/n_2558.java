7e1016ea864c471efe803d389bb4e222b6b1413a
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-25 11:18:05 +0900

    백준 2558번

---
+++ src/baekjoon/n_2558.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class n_2558 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
+
+
