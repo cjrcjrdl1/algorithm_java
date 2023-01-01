8ae88287470a243c26fdbdba689b78a98df885ec
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-01 10:20:57 +0900

    백준 10872번

---
+++ src/baekjoon/n_10872.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class n_10872 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact*=i;
+        }
+        System.out.println(fact);
+    }
+}
+
+
