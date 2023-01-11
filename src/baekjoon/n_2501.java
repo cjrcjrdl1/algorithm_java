62e54868d198168d78b41696831c334a7855314d
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-11 23:29:43 +0900

    백준 2501번

---
+++ src/baekjoon/n_2501.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n_2501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        int result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                count++;
+            }
+
+            if (K == count) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
+
+
