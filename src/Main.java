311caa23e99aa6892e6681f5879f5d8f4d813c55
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-19 10:00:00 +0000

    now

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public String solution(int A, int B, int C) {
-        int min = A * 60 + B + C;
-
-        int hour = (min / 60) % 24;
-        int minute = min % 60;
-
-        return hour + " " + minute;
-    }
     public static void main(String[] args) {
-        Main T = new Main();
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        int C = scanner.nextInt();
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
 
-        System.out.println(T.solution(A, B, C));
     }
 }
+
 
 
