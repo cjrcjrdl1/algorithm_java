1e9e17bb39e3e571a9ba062b44866ba0337fc1bf
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-20 19:53:53 +0900

    백준 5338번

---
+++ src/baekjoon/n_5338.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+public class n_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+
+    }
+}
+
+
+
