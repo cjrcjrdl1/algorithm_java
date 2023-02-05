03dca7feb0171974b9df5aef7dda94950933dd76
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-05 13:31:13 +0900

    백준 5337번

---
+++ src/baekjoon/n_5337.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+public class n_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
+
+
+
