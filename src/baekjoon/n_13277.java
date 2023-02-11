a0daddc1ba3cd07a5afd798cb593909425106e92
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-11 14:50:23 +0900

    백준 13277번

---
+++ src/baekjoon/n_13277.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class n_13277 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+
+        System.out.println(a.multiply(b));
+    }
+}
+
+
+
