e3dacc086b15cc1d53d7fcec9b7db451ac09febb
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-21 14:01:50 +0900

    String_1

--- src/java_tutorial/String_1.java
+++ src/java_tutorial/String_1.java
@@ -1,29 +1,32 @@
-package study_string;
+package java_tutorial;
 
 import java.util.Scanner;
 
-public class exam1 {
-    public int solution(String str, char c) {
+public class String_1 {
+
+    public int solution(String str, char t) {
         int answer = 0;
         str = str.toUpperCase();
-        c = Character.toUpperCase(c);
+        t = Character.toUpperCase(t);
 
-        for (char x : str.toCharArray()) {
-            if (x == c) {
-                answer++;
+        for(char x : str.toCharArray()){
+            if(t==x){
+                answer+=1;
             }
         }
 
         return answer;
     }
     public static void main(String[] args) {
-        exam1 T = new exam1();
+        String_1 T = new String_1();
         Scanner scanner = new Scanner(System.in);
+
         String str = scanner.next();
         char c = scanner.next().charAt(0);
 
-        System.out.println(T.solution(str, c));
+        System.out.println(T.solution(str,c));
     }
 }
+
 
 
