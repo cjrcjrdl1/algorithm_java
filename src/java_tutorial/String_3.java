939359be37f5c84ba775873a5e680cd687b40174
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-23 00:16:15 +0900

    String_3

---
+++ src/java_tutorial/String_3.java
@@ -0,0 +1,31 @@
+package java_tutorial;
+
+import java.util.Scanner;
+
+public class String_3 {
+
+    public String solution(String str) {
+        String answer = "";
+        int max = Integer.MIN_VALUE;
+        String[] split = str.split(" ");
+        for (String s : split) {
+            if (max < s.length()) {
+                max = s.length();
+                answer = s;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        String_3 T = new String_3();
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+
+        System.out.println(T.solution(str));
+    }
+}
+
+
+
