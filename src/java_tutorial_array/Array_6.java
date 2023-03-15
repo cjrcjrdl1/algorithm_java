ee8791b86080d89c0d02005b3f7b79eab416e546
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-03-15 18:50:46 +0900

    Array_6

---
+++ src/java_tutorial_array/Array_6.java
@@ -0,0 +1,55 @@
+package java_tutorial_array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array_6 {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+            if (isPrime(res)) {
+                answer.add(res);
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isPrime(int num) {
+        if (num==1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Array_6 T = new Array_6();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+
+}
+
+
+
