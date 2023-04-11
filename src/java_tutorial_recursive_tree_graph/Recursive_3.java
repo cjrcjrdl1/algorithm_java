93f874b693a73e554741f0aec9c4c4a2726239d1
springjoa <cjrcjrdl1@gmail.com>
2023-04-11 12:45:04 +0900

    Recursive_3

---
+++ src/java_tutorial_recursive_tree_graph/Recursive_3.java
@@ -0,0 +1,20 @@
+package java_tutorial_recursive_tree_graph;
+
+public class Recursive_3 {
+    public int DFS(int n) {
+        if (n==1) return 1;
+        else return n * DFS(n - 1);
+    }
+
+
+
+    public static void main(String[] args) {
+        Recursive_3 T = new Recursive_3();
+        System.out.println(T.DFS(5));
+
+    }
+
+}
+
+
+
