b0b349d9a8459c3f1b9fc93d450e3a644b71fc2a
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-05-21 10:51:24 +0900

    Recursive_5

---
+++ src/java_tutorial_recursive_tree_graph/Recursive_5.java
@@ -0,0 +1,43 @@
+package java_tutorial_recursive_tree_graph;
+
+class Node{
+    int data;
+    Node lt,rt;
+
+    public Node(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
+public class Recursive_5 {
+    Node root;
+
+    public void DFS(Node root) {
+        if (root == null) {
+            return;
+        } else {
+//            System.out.print(root.data + " "); //전위순회
+            DFS(root.lt);
+//            System.out.print(root.data + " "); //중위순회
+            DFS(root.rt);
+            System.out.print(root.data + " "); //후위순회
+        }
+    }
+
+    public static void main(String[] args) {
+        Recursive_5 tree = new Recursive_5();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.DFS(tree.root);
+
+    }
+
+}
+
+
+
