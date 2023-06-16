7759bb6e349b494ee307169308cbcab3ab82c598
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-06-16 20:52:16 +0900

    Recursive_10

---
+++ src/java_tutorial_recursive_tree_graph/Recursive_10.java
@@ -0,0 +1,48 @@
+package java_tutorial_recursive_tree_graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//class Node{
+//    int data;
+//    Node lt,rt;
+//
+//    public Node(int val) {
+//        data = val;
+//        lt = rt = null;
+//    }
+//}
+
+
+public class Recursive_10 {
+    Node root;
+    public int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                if (cur.lt==null && cur.rt==null) return L;
+                if (cur.lt!=null) Q.offer(cur.lt);
+                if (cur.rt!=null) Q.offer(cur.rt);
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Recursive_10 tree = new Recursive_10();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.BFS(tree.root));
+    }
+}
+
+
+
